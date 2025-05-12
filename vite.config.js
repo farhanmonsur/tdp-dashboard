f82fd42dbbc7ace04f7beb8d71670f75db775083
Farhan Monsur <farhan.i.monsur@gmail.com>
2025-05-12 18:18:29 +1000

    base url fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tdp-dashboard',
+  base: '/tdp-dashboard/',
   plugins: [react()],
 })
